c9db25801b976b2f78f5174a7117d7157eadff97
zhouxinyao <zhouxinyao@zbj.com>
2018-07-25 19:27:24 +0800

    新建spirngboot项目（测试sso）

---
+++ mukewang/sso_yaoyao1/ssoclient-app1/src/main/java/com/yy/ssoclient_app1/ssoclientapp1/SsoclientApp1Application.java
@@ -0,0 +1,12 @@
+package com.yy.ssoclient_app1.ssoclientapp1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsoclientApp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsoclientApp1Application.class, args);
+	}
+}
---
+++ mukewang/sso_yaoyao1/ssoclient-app2/src/main/java/com/yy/ssoclientapp2/ssoclientapp2/SsoclientApp2Application.java
@@ -0,0 +1,12 @@
+package com.yy.ssoclientapp2.ssoclientapp2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsoclientApp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsoclientApp2Application.class, args);
+	}
+}
