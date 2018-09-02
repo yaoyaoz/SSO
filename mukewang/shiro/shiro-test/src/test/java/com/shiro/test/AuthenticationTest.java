86ce457b53ab9e1f59c92cccca66ac62fd4d5679
yaoyaoz <905440208@qq.com>
2018-09-02 13:35:18 +0800

    shiro登录、登出

---
+++ mukewang/shiro/shiro-test/src/test/java/com/shiro/test/AuthenticationTest.java
@@ -0,0 +1,48 @@
+package com.shiro.test;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.mgt.DefaultSecurityManager;
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.realm.SimpleAccountRealm;
+import org.apache.shiro.subject.Subject;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ *
+ *
+ * Created by yaoyao on 2018-08-25.
+ */
+public class AuthenticationTest {
+
+    SimpleAccountRealm simpleAccountRealm = new SimpleAccountRealm();
+
+    @Before
+    public void addUser() {
+        simpleAccountRealm.addAccount("yaoyao", "123456");
+    }
+
+    @Test
+    public void testAuthentication() {
+
+        //1、构建SecurityManager环境
+        DefaultSecurityManager defaultSecurityManager = new DefaultSecurityManager();
+        defaultSecurityManager.setRealm(simpleAccountRealm);
+
+        //2、主体提交认证请求
+        SecurityUtils.setSecurityManager(defaultSecurityManager);
+        Subject subject = SecurityUtils.getSubject();
+
+        AuthenticationToken token = new UsernamePasswordToken("yaoyao", "123456");
+
+        subject.login(token);
+        System.out.println("isAuthenticated:" + subject.isAuthenticated());
+
+        subject.logout();
+        System.out.println("isAuthenticated:" + subject.isAuthenticated());
+
+    }
+
+}
