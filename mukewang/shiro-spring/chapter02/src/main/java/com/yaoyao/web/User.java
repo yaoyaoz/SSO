bc9643226c131589fd2ea6b11d8a25b5f17e1605
zhouxinyao <zhouxinyao@zbj.com>
2018-09-27 18:37:17 +0800

    spring-shiro:controller

---
+++ mukewang/shiro-spring/chapter02/src/main/java/com/yaoyao/web/User.java
@@ -0,0 +1,27 @@
+package com.yaoyao.web;
+
+/**
+ * Created by yaoyao on 2018/9/27.
+ */
+public class User {
+
+    private String userName;
+
+    private String password;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ mukewang/shiro-spring/chapter02/src/main/java/com/yaoyao/web/UserController.java
@@ -0,0 +1,31 @@
+package com.yaoyao.web;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.subject.Subject;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by yaoyao on 2018/9/27.
+ */
+@Controller
+public class UserController {
+
+    @RequestMapping(value = "/subLogin.html")//method = RequestMethod.POST
+    @ResponseBody
+    public String subLogin(User user) {
+        Subject subject = SecurityUtils.getSubject();
+        UsernamePasswordToken token = new UsernamePasswordToken(user.getUserName(), user.getPassword());
+        try {
+            subject.login(token);
+        } catch (AuthenticationException e) {
+            return e.getMessage();
+        }
+        return "登录成功";
+    }
+
+}
